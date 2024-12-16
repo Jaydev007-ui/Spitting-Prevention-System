584808dda979f3697e7212d8ccd1e28bf78a0114
Jaydev007-ui <jaydevzala07@gmail.com>
2024-12-16 10:31:05 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -50,7 +50,7 @@ uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"]
 
 # Input for GitHub credentials
 username = "Jaydev007-ui"
-token = "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"
+token = "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"
 user_email = "jaydevzala07@gmail.com"
 user_name = username
 
